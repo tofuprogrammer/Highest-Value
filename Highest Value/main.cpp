94f6f0daa63256b00520f7276409424ff67fb51a
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-11 00:39:57 +0000

    Added comments Also changed the starting value for highestValue

--- Highest Value/main.cpp
+++ Highest Value/main.cpp
@@ -2,14 +2,14 @@
 
 int main() {
     float userNumbers[10];
-    float highestValue = -1000000;
+    float highestValue = -INFINITY; // Initialised with a value to prevent odd behaviour
     for (int numberOfInputs = 0; numberOfInputs < 10; numberOfInputs++){
         std::cout << "Enter a number: ";
         std::cin >> userNumbers[numberOfInputs];
     }
     for (int numberOfInputs = 0; numberOfInputs < 10; numberOfInputs++){
         if (userNumbers[numberOfInputs] > highestValue){
-            highestValue = userNumbers[numberOfInputs];
+            highestValue = userNumbers[numberOfInputs]; // Overwrites highest number with user's input
         }
     }
     std::cout << "The highest value you entered is " << highestValue << '\n';
